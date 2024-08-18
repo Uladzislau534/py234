7a35a3ef960775c297ae58da8b5931d6657942ca
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 19:15:51 +0300

    3_end

--- Python3/Python3.py
+++ Python3/Python3.py
@@ -1,1 +1,31 @@
+def create_synonym_dictionary(n):
+    synonyms = {}
+
+    for i in range(n):
+        pair = input(f"Пара {i+1}: ").split(" — ")
+        word1 = pair[0].strip().lower()
+        word2 = pair[1].strip().lower()
+
+        synonyms[word1] = word2
+        synonyms[word2] = word1
+
+    return synonyms
+
+def find_synonym(synonyms):
+    while True:
+        word = input("Введите слово: ").strip().lower()
+        if word in synonyms:
+            print(f"Синоним: {synonyms[word].capitalize()}")
+            break
+        else:
+            print("Такого слова в словаре нет. Попробуйте ещё раз.")
+
+# Ввод данных
+n = int(input("Введите количество пар слов: "))
+
+# Создание словаря синонимов
+synonyms = create_synonym_dictionary(n)
+
+# Поиск синонима
+find_synonym(synonyms)
 
